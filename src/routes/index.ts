b0040fc55a9e3c59a930f65686cc0ed53616ebf5
Matias-DR <Matias-DR@outlook.com>
2023-01-12 13:44:53 -0300

    Add - index.ts with all routes for ctrler added

---
+++ src/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router, Request, Response } from 'express';
+import { UserCtrler } from '../controllers/user-ctrler';
+
+class Routes {
+    private router: Router;
+    private usrCtrler: UserCtrler;
+
+    constructor() {
+        this.router = Router();
+        this.usrCtrler = new UserCtrler();
+    }
+
+    config(): void {
+        this.router.post('/get_users', this.usrCtrler.getUsers)
+        this.router.post('/get_user', this.usrCtrler.getUser)
+        this.router.post('/add_user', this.usrCtrler.addUser)
+        this.router.post('/edit_user', this.usrCtrler.editUser)
+        this.router.post('/del_user', this.usrCtrler.delUser)
+    }
+}
